0c1d204315246ea01cdd5cc1daab4a12ad8e9428
Rayen HAMADI <rayenhamadi48@gmail.com>
2024-09-25 11:48:33 +0200

    Test for Machine

--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -1,1 +1,11 @@
 # Create your tests here.
+from django.test import TestCase
+from .models import Machine
+
+
+class MachineModelTests(TestCase):
+    def test_machine_creation(self):
+        self.assertEqual(Machine.objects.count(), 0)
+        Machine.objects.create(nom="scie", prix=1000, n_serie="16832")
+        self.assertEqual(Machine.objects.count(), 1)
+        # basculer entre 1 et 2 Pour mieux visualiser et interpreter le test
