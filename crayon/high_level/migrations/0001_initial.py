616641c563f36eaa8a228f3aff28a7a43752d0aa
Rayen <rayenhamadi48@gmail.com>
2024-10-01 09:12:19 +0200

    Definition of object done & corrections models.py

--- crayon/high_level/migrations/0001_initial.py
+++ crayon/high_level/migrations/0001_initial.py
@@ -5,108 +5,222 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Machine',
+            name="Machine",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nom', models.CharField(max_length=100)),
-                ('prix', models.IntegerField()),
-                ('n_serie', models.IntegerField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nom", models.CharField(max_length=100)),
+                ("prix", models.IntegerField()),
+                ("n_serie", models.IntegerField()),
             ],
         ),
         migrations.CreateModel(
-            name='QuantiteRessource',
+            name="QuantiteRessource",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('quantite', models.IntegerField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("quantite", models.IntegerField()),
             ],
         ),
         migrations.CreateModel(
-            name='Ressource',
+            name="Ressource",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nom', models.CharField(max_length=100)),
-                ('prix', models.IntegerField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nom", models.CharField(max_length=100)),
+                ("prix", models.IntegerField()),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='Ville',
+            name="Ville",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nom', models.CharField(max_length=100)),
-                ('code_postal', models.IntegerField()),
-                ('prix_m_2', models.IntegerField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nom", models.CharField(max_length=100)),
+                ("code_postal", models.IntegerField()),
+                ("prix_m_2", models.IntegerField()),
             ],
         ),
         migrations.CreateModel(
-            name='Etape',
+            name="Etape",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nom', models.CharField(max_length=100)),
-                ('duree', models.IntegerField()),
-                ('etape_suivante', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='high_level.etape')),
-                ('machine', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='high_level.machine')),
-                ('quantite_ressource', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='high_level.quantiteressource')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nom", models.CharField(max_length=100)),
+                ("duree", models.IntegerField()),
+                (
+                    "etape_suivante",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="high_level.etape",
+                    ),
+                ),
+                (
+                    "machine",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="high_level.machine",
+                    ),
+                ),
+                (
+                    "quantite_ressource",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="high_level.quantiteressource",
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Produit',
+            name="Produit",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nom', models.CharField(max_length=100)),
-                ('prix', models.IntegerField()),
-                ('premiere_etape', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='high_level.etape')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nom", models.CharField(max_length=100)),
+                ("prix", models.IntegerField()),
+                (
+                    "premiere_etape",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="high_level.etape",
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.AddField(
-            model_name='quantiteressource',
-            name='ressource',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='high_level.ressource'),
+            model_name="quantiteressource",
+            name="ressource",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.PROTECT, to="high_level.ressource"
+            ),
         ),
         migrations.CreateModel(
-            name='Stock',
+            name="Stock",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nombre', models.IntegerField()),
-                ('ressource', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='high_level.ressource')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nombre", models.IntegerField()),
+                (
+                    "ressource",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="high_level.ressource",
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Usine',
+            name="Usine",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nom', models.CharField(max_length=100)),
-                ('surface', models.IntegerField()),
-                ('machines', models.ManyToManyField(to='high_level.machine')),
-                ('ville', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='high_level.ville')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nom", models.CharField(max_length=100)),
+                ("surface", models.IntegerField()),
+                ("machines", models.ManyToManyField(to="high_level.machine")),
+                (
+                    "ville",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="high_level.ville",
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='SiegeSocial',
+            name="SiegeSocial",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nom', models.CharField(max_length=100)),
-                ('surface', models.IntegerField()),
-                ('ville', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='high_level.ville')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nom", models.CharField(max_length=100)),
+                ("surface", models.IntegerField()),
+                (
+                    "ville",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to="high_level.ville",
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,14 +1,19 @@
 # Create your models here.
 from django.db import models
 
+
 # Modèle représentant une ville avec nom, code postal et prix au mètre carré
 class Ville(models.Model):
     nom = models.CharField(max_length=100)
     code_postal = models.IntegerField()
     prix_m_2 = models.IntegerField()
-    
-    
+
+    def __str__(self):
+        return self.nom
+
+
 # Classes abstraites
+
 
 # Modèle abstrait pour les locaux (ex : usine, siège social)
 class Local(models.Model):
@@ -16,14 +21,14 @@ class Local(models.Model):
     surface = models.IntegerField()
     ville = models.ForeignKey(
         Ville,  # Association avec une ville
-        on_delete=models.PROTECT, # La suppression d'une ville n'entraînera pas celle du local
+        on_delete=models.PROTECT,  # La suppression d'une ville n'entraînera pas celle du local
     )
 
     def __str__(self):
         return self.nom
 
     class Meta:
-        abstract = True # Indique que cette classe est abstraite
+        abstract = True  # Indique que cette classe est abstraite
 
 
 # Modèle abstrait pour les objets (ex : ressource, produit)
@@ -39,6 +44,7 @@ class Objet(models.Model):
 
 
 # Classes concrètes (non-abstraites)
+
 
 # Modèle représentant un siège social, héritant de la classe Local
 class SiegeSocial(Local):
@@ -74,7 +80,7 @@ class QuantiteRessource(models.Model):
     )
 
     def __str__(self):
-        return self.ressource
+        return self.ressource.nom
 
 
 # Modèle représentant le stock d'un objet
@@ -86,7 +92,7 @@ class Stock(models.Model):
     nombre = models.IntegerField()
 
     def __str__(self):
-        return self.objet
+        return self.ressource.nom
 
 
 # Modèle représentant une étape dans un processus de production
@@ -94,7 +100,7 @@ class Etape(models.Model):
     nom = models.CharField(max_length=100)  # Nom de l'étape
     duree = models.IntegerField()  # Durée de l'étape
     quantite_ressource = models.ForeignKey(
-        QuantiteRessource, # Ressource requise pour cette étape
+        QuantiteRessource,  # Ressource requise pour cette étape
         on_delete=models.PROTECT,  # La suppression d'une ressource n'entraîne pas celle de l'étape
     )
 
@@ -105,16 +111,18 @@ class Etape(models.Model):
 
     etape_suivante = models.ForeignKey(
         "self",  # Référence à l'étape suivante (relation récursive vers la classe elle meme)
+        blank=True,
+        null=True,
         on_delete=models.PROTECT,
     )
 
     def __str__(self):
         return self.nom
-        
-        
+
+
 # Modèle représentant un produit qui a une première étape de production
 class Produit(Objet):
     premiere_etape = models.ForeignKey(
         Etape,  # Première étape de fabrication du produit
-        on_delete=models.PROTECT,  
+        on_delete=models.PROTECT,
     )
