1d5bf7e31b7efa3cc2ef89e19b40f71df25b8dbf
Rayen <rayenhamadi48@gmail.com>
2024-10-04 16:48:11 +0200

    blocked into class relations c++

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -8,6 +8,7 @@ using json = nlohmann::json;
 using namespace std;
 
 class Ville {
+ public:
   string nom;
   int code_postal;
   float prix_m_2;
@@ -20,6 +21,25 @@ class Ville {
     prix_m_2 = p;
   }
 
+  // Constructeur JSON data
+  Ville(json data) : Ville(data["nom"], data["CP"], data["Prix/m^2"]) {}
+
+  // Constructeur ID qui fera lui meme la requette
+  Ville(int id) {
+    cpr::Response r =
+        cpr::Get(cpr::Url{"http://localhost:8000/Ville/" + to_string(id)});
+    r.status_code;  // 200 Ok si 404 not found
+    r.text;         // JSON text string
+
+    json data = json::parse(r.text);
+
+    // Ville(data);
+
+    nom = data["Nom"];
+    code_postal = data["CP"];
+    prix_m_2 = data["Prix/m^2"];
+  }
+
   // Methodes
   //  methode equivalente à la methode str en models.py
   friend std::ostream& operator<<(std::ostream& out, const Ville& v) {
@@ -27,24 +47,49 @@ class Ville {
   }
 };
 
+class Local {
+ public:
+  string nom;
+  int surface;
+  Ville* ville;  // Pointeur vers une ville
+};
+
+// Classe abstraite Objet
+class Objet {
+ public:
+  string nom;
+  int prix;
+};
+
+/*
+std::unique_ptr<A> pa = std::make_unique<A>(..... parametre du const ....);
+const auto pa = std::make_unique<A>(......) dans la vrai vie
+*/
+
 // Fonction main
 auto main() -> int {
   const auto v = Ville("Toulouse", 31400, 2000);
   std::cout << "Ville: " << v << std ::endl;
 
-  cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8000/Ville/1/"});
+  /*
+    cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8000/Ville/1/"});
 
-  r.status_code;             // 200 Ok si 404 not found
-  r.header["content-type"];  // application/json; charset=utf-8
-  r.text;                    // JSON text string
 
-  std::cout << r.status_code
-            << std::endl;  // verification si status code est OK ou pas
-  std::cout << r.text << std::endl;
+    r.status_code;             // 200 Ok si 404 not found
+    r.header["content-type"];  // application/json; charset=utf-8
+    r.text;                    // JSON text string
 
-  json ville = json::parse(r.text);
-  // verification
-  std::cout << ville["CP"] << std::endl;  // Regarder le Readme JSON
+    std::cout << r.status_code
+              << std::endl;  // verification si status code est OK ou pas
+    std::cout << r.text << std::endl;
+
+    json ville = json::parse(r.text);
+    // verification
+    std::cout << ville["CP"] << std::endl;  // Regarder le Readme JSON
+
+  */
+  const auto v1 = Ville(1);
+  std::cout << v1 << std::endl;
 
   return 0;
 }
