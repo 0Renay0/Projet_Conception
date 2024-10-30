0518f856cc01ed04b89c7e8b3bedfb02800b6ae9
Rayen <rayenhamadi48@gmail.com>
2024-10-30 13:24:53 +0100

    Api fixed

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -123,9 +123,77 @@ class Machine(models.Model):
         return self.json()
 
 
+
+# Modèle représentant une étape dans un processus de production
+class Etape(models.Model):
+    nom = models.CharField(max_length=100)  # Nom de l'étape
+    duree = models.IntegerField()  # Durée de l'étape
+    quantite_ressource = models.ForeignKey(
+        QuantiteRessource,  # Ressource requise pour cette étape
+        on_delete=models.PROTECT,  # La suppression d'une ressource n'entraîne pas celle de l'étape
+        blank=True,
+        null=True,
+        
+    )
+
+    machine = models.ForeignKey(
+        Machine,  # Machine utilisée pour cette étape
+        on_delete=models.PROTECT,  # La suppression d'une machine n'entraîne pas celle de l'étape
+    )
+
+    etape_suivante = models.ForeignKey(
+        "self",  # Référence à l'étape suivante (relation récursive vers la classe elle meme)
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return self.nom
+        
+    def json(self):
+    	d = {
+    		"Nom": self.nom,
+    		"Durée": self.duree,
+    		"Quantité necessaire": self.quantite_ressource.id if self.quantite_ressource else None,
+    		"Machine ID": self.machine.id if self.machine else None,
+    		"Etape suivante ID": self.etape_suivante.id if self.etape_suivante else None,
+    	}
+    	return d
+    
+    def json_extended(self):
+        d = {
+            "Nom": self.nom,
+            "Durée": self.duree,
+            "Quantité necessaire": self.quantite_ressource.json_extended() if self.quantite_ressource else None,
+            "Machine": self.machine.json_extended() if self.machine else None,
+            "Etape suivante": self.etape_suivante.json_extended() if self.etape_suivante else None,
+        }
+        return d
+
+
+# Modèle représentant un produit qui a une première étape de production
+class Produit(Objet):
+    premiere_etape = models.ForeignKey(
+        Etape,  # Première étape de fabrication du produit
+        on_delete=models.PROTECT,
+    )
+
+    def json(self):
+        d = {"Premiere etape": self.premiere_etape.id}
+        return d
+
+    def json_extended(self):
+        d = {
+            "Premiere etape": self.premiere_etape.json_extended(),
+        }
+        return d
+        
+        
 # Modèle représentant une usine, qui possède plusieurs machines
 class Usine(Local):
     machines = models.ManyToManyField(Machine)
+    produit = models.ManyToManyField(Produit)
 
     def costs(self):
         Prix_terrain = self.ville.prix_m_2 * self.surface
@@ -163,6 +231,7 @@ class Usine(Local):
     def json(self):
         d = {
             "Machine": [machines.pk for machines in Machine.objects.all()],
+            "Produit": [produit.pk for produit in Produit.objects.all()],
         }
         return d
 
@@ -172,10 +241,11 @@ class Usine(Local):
             "Ville": self.ville.json_extended(),
             "Surface": self.surface,
             "Machine": [machines.json_extended() for machines in Machine.objects.all()],
+            "Produit": [produit.json_extended() for produit in Produit.objects.all()],
         }
         return d
-
-
+        
+        
 # Modèle représentant le stock d'un objet
 class Stock(models.Model):
     ressource = models.ForeignKey(
@@ -207,72 +277,7 @@ class Stock(models.Model):
         d = {
             "Ressource": self.ressource.json_extended(),
             "Usine": self.usine.json_extended(),
-            "Nombre": self.nombre,
+            "Nombre": self.nombre.json_extended(),
         }
         return d
 
-
-# Modèle représentant une étape dans un processus de production
-class Etape(models.Model):
-    nom = models.CharField(max_length=100)  # Nom de l'étape
-    duree = models.IntegerField()  # Durée de l'étape
-    quantite_ressource = models.ForeignKey(
-        QuantiteRessource,  # Ressource requise pour cette étape
-        on_delete=models.PROTECT,  # La suppression d'une ressource n'entraîne pas celle de l'étape
-        blank=True,
-        null=True,
-        
-    )
-
-    machine = models.ForeignKey(
-        Machine,  # Machine utilisée pour cette étape
-        on_delete=models.PROTECT,  # La suppression d'une machine n'entraîne pas celle de l'étape
-    )
-
-    etape_suivante = models.ForeignKey(
-        "self",  # Référence à l'étape suivante (relation récursive vers la classe elle meme)
-        blank=True,
-        null=True,
-        on_delete=models.PROTECT,
-    )
-
-    def __str__(self):
-        return self.nom
-
-    def json(self):
-        d = {
-            "Nom": self.nom,
-            "Durée": self.duree,
-            "Quantité necessaire": self.quantite_ressource.id,
-            "Machine ID": self.machine.id,
-            "Etape suivante ID": self.etape_suivante.id,
-        }
-        return d
-
-    def json_extended(self):
-        d = {
-            "Nom": self.nom,
-            "Durée": self.duree,
-            "Quantité necessaire": self.quantite_ressource.json_extended(),
-            "Machine": self.machine.json_extended(),
-            "Etape suivante": self.etape_suivante.json_extended(),
-        }
-        return d
-
-
-# Modèle représentant un produit qui a une première étape de production
-class Produit(Objet):
-    premiere_etape = models.ForeignKey(
-        Etape,  # Première étape de fabrication du produit
-        on_delete=models.PROTECT,
-    )
-
-    def json(self):
-        d = {"Premiere etape": self.premiere_etape.id}
-        return d
-
-    def json_extended(self):
-        d = {
-            "Premiere etape": self.premiere_etape.json_extended(),
-        }
-        return d
